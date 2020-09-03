e21636857c84eff24af41f6f637d94ef1dcf3e2f
pape018 <61976319+pape018@users.noreply.github.com>
2020-09-03 10:27:30 +0200

    Hej

--- Pavel/Pavel/Program.cs
+++ Pavel/Pavel/Program.cs
@@ -7,6 +7,7 @@ namespace Pavel
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("hello niklas");
         }
     }
 }
