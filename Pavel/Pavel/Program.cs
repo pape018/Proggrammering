90b5d9cfd6be9244f459a59f3411c7fa3efc321d
pape018 <61976319+pape018@users.noreply.github.com>
2020-09-03 10:19:31 +0200

    x

---
+++ Pavel/Pavel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pavel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
