6d45e852dc6e372fa0683704b53dcee63f69960d
pape018 <61976319+pape018@users.noreply.github.com>
2020-09-10 10:51:05 +0200

    yes

--- Pavel/Pavel/Program.cs
+++ Pavel/Pavel/Program.cs
@@ -8,6 +8,12 @@ namespace Pavel
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("hello niklas");
+            Console.WriteLine("What is yo name");
+            String Name = Console.ReadLine();
+            Console.WriteLine("How old are you");
+            int age = Console.ReadLine();
+            Console.WriteLine("Are you alive");
+
         }
     }
 }
